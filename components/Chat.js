009fa14924c71e58ecbaf9bc229228e5f902ea39
ozkalai <melihozkalayy@gmail.com>
2022-10-08 00:29:40 +0300

    feat: chat component with retrieve data

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,32 @@
+import { useState, useEffect } from "react";
+
 const Chat = ({ supabase }) => {
-  console.log("supabase", supabase);
-  return <div></div>;
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      const { data: initialMessages, error } = await supabase
+        .from("message")
+        .select("*")
+        .order("id", { ascending: true });
+      if (error) {
+        console.log(error);
+      }
+      setMessages(initialMessages);
+    };
+
+    getMessages();
+  }, []);
+
+  return (
+    <div>
+      {messages.map((message) => (
+        <div key={message.id}>
+          <p>{message.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Chat;
